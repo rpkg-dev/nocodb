4c5f791f0456c04a8f227a367685a473aa48b80d
Salim B <git@salim.space>
2024-05-28 00:16:14 +0200

    chore: initial commit

---
+++ R/nocodb-package.R
@@ -0,0 +1,15 @@
+#' @keywords internal
+#' @importFrom magrittr %>%
+#' @importFrom magrittr %<>%
+#' @importFrom magrittr %T>%
+#' @importFrom magrittr %!>%
+#' @importFrom magrittr %$%
+#' @importFrom rlang %|%
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#' @importFrom lifecycle deprecated
+## usethis namespace: end
+NULL
---
+++ man/roxygen/meta.R
@@ -0,0 +1,8 @@
+list(
+  rd_family_title = list(attachments = "Other functions to manage NocoDB attachments:",
+                         bases       = "Other functions to manage NocoDB bases:",
+                         cols        = "Other functions to manage NocoDB table columns:",
+                         common      = "Other common functions:",
+                         tbls        = "Other functions to manage NocoDB tables:",
+                         users       = "Other functions to manage NocoDB users:")
+)
