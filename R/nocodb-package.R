923b4adc3e01e2e68876788675817d5b9562e585
Salim B <git@salim.space>
2025-03-03 06:43:35 +0100

    refactor: simplify code

--- R/nocodb-package.R
+++ R/nocodb-package.R
@@ -1,9 +1,9 @@
 #' @section Package configuration:
 #'
-#' `r pkgsnip::md_snip(id = "pkg_config", pkg = this_pkg)`
+#' `r pkgsnip::md_snip(id = "pkg_config")`
 #'
 #' ```{r, echo = FALSE}
-#' pal::print_pkg_config(this_pkg)
+#' pal::print_pkg_config()
 #' ```
 #'
 #' @keywords internal
